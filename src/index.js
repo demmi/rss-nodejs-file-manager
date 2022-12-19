0a06963a9b6896e48a16c7e517b8d89bd989a76f
Andrew Gerasimchik <gerasimchick@tut.by>
2022-12-19 16:29:38 +0200

    feat: add os section commands

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { parseArgs } from './utils/args.js'
-import { createInterface } from "node:readline";
+import { createInterface } from 'node:readline'
+import os from 'node:os'
 
 const currentUser = parseArgs()
+
 async function greeting(username) {
-    process.stdout.write(`Welcome to the File Manager, ${username}!\n`);
+    process.stdout.write(`Welcome to the File Manager, ${username}!\n`)
 }
 
 async function runner() {
@@ -12,10 +14,41 @@ async function runner() {
     const inputInterface = createInterface({
         input: process.stdin,
         output: process.stdout,
-    });
+    })
+
+    inputInterface.on('line', async (input) => {
+        const command = input.trim()
+        switch (command) {
+            case '.exit':
+                inputInterface.write(
+                    `Thank you for using File Manager, ${currentUser}!`
+                )
+                process.exit()
+                break
+            case 'os --EOL':
+                inputInterface.write(JSON.stringify(os.EOL) + '\n')
+                break
+            case 'os --username':
+                inputInterface.write(os.userInfo().username + '\n')
+                break
+            case 'os --homedir':
+                inputInterface.write(os.homedir() + '\n')
+                break
+            case 'os --architecture':
+                inputInterface.write(os.arch() + '\n')
+                break
+            case 'os --cpus':
+                os.cpus().forEach((cpu) =>
+                    inputInterface.write(cpu.model + '\n')
+                )
+                break
+        }
+    })
 
     inputInterface.on('SIGINT', () => {
-        inputInterface.write(`Thank you for using File Manager, ${currentUser}!`)
+        inputInterface.write(
+            `Thank you for using File Manager, ${currentUser}!`
+        )
         process.exit()
     })
 }
